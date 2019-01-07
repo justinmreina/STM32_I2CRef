0897ff589ef982f9c6eff0785ebb1d969bf0558f
Justin Reina <justinmreina@gmail.com>
2019-01-06 23:06:41 -0800

    (C)    decrease i2c sample clock by increasing prescaler field of        timinggr register

--- src/main.c
+++ src/main.c
@@ -143,7 +143,7 @@ void SystemClock_Config(void) {
 void MX_I2C2_Init(void) {
 
   hi2c2.Instance = I2C2;
-  hi2c2.Init.Timing = 0x20303E5D;
+  hi2c2.Init.Timing = 0xF0303E5D;
   hi2c2.Init.OwnAddress1 = 200;
   hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
